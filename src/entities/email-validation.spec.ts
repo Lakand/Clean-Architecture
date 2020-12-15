af9c6447ee5c85ebaa5bdef883e4fb3660313b67
Celso Gabriel Vieira Ribeiro Lopes <c.gabriel.vieira@hotmail.com>
2020-12-15 19:32:06 -0300

    test: validate emails with domain parts larger than 63 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -40,4 +40,9 @@ describe('Email validation', () => {
     const email = 'any@'
     expect(Email.validade(email)).toBeFalsy()
   })
+
+  test('should not accept domain with a part larger than 63 chars', () => {
+    const email = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validade(email)).toBeFalsy()
+  })
 })
