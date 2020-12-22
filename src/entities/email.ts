3dbbf7f7a2148ca90ec2e1c5345460dd5e1581db
Celso Gabriel Vieira Ribeiro Lopes <c.gabriel.vieira@hotmail.com>
2020-12-22 15:55:18 -0300

    feat: verify e-mail adress when creating a user

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,21 @@
+import { Either, left, right } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+
 export class Email {
+  private readonly email: string
+
+  constructor (email: string) {
+    this.email = email
+  }
+
+  static create (email: string): Either<InvalidEmailError, Email> {
+    if (Email.validade(email)) {
+      return right(new Email(email))
+    }
+
+    return left(new InvalidEmailError())
+  }
+
   static validade (email: string): boolean {
     if (!email) {
       return false
