80896b33c091176ce474c275cc0d74b60dcaf828
Celso Gabriel Vieira Ribeiro Lopes <c.gabriel.vieira@hotmail.com>
2021-02-10 19:45:18 -0300

    refactor: organize imports

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,5 +1,6 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType } from '@/main/config/middleware/'
+
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
